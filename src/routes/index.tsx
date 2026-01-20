5536b764d93e0ecb128d70c023a59b77a92ef1a0
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2026-01-20 20:09:13 +0300

    adde the code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,7 @@ import App from "@/App";
 import Home from "@/components/layout/Home";
 import Login from "@/pages/Login";
 import NotFound from "@/pages/NotFound";
-import Profile from "@/pages/Profile";
-import Register from "@/pages/register";
 
-import UnAuthorized from "@/pages/UnAuthorized";
-import Verify from "@/pages/Verify";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
@@ -23,22 +19,6 @@ export const router = createBrowserRouter([
   {
     path: "/login",
     Component: Login,
-  },
-  {
-    path: "/register",
-    Component: Register,
-  },
-  {
-    path: "/verify",
-    Component: Verify,
-  },
-  {
-    path: "/unauthorized",
-    Component: UnAuthorized,
-  },
-  {
-    path: "/profile",
-    Component: Profile,
   },
 
   {
